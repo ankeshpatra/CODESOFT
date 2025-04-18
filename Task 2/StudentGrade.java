f8b116705ac014593bd52aca1cbbe4293edd1c72
ankeshpatra <ankeshpatra.2005@gmail.com>
2025-04-18 17:12:01 +0530

    first commit

---
+++ Task 2/StudentGrade.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StudentGrade {
+    public static void main(String[] args) {
+        int n, totalMarks = 0;
+        double averageMarks = 0.0;
+        ArrayList<Integer> marks = new ArrayList<>();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of students: ");
+        n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int mark;
+            while (true) {
+                System.out.print("Enter the marks of the subject " + (i + 1) + " (0 to 100): ");
+                mark = sc.nextInt();
+                if (mark >= 0 && mark <= 100) {
+                    marks.add(mark);
+                    break;
+                } else {
+                    System.out.println("❌ Invalid input. Please enter marks between 0 and 100.");
+                }
+            }
+        }
+
+        for (int mark : marks) {
+            totalMarks += mark;
+        }
+
+        averageMarks = (double) totalMarks / n;
+
+        char grade;
+        if (averageMarks >= 90 && averageMarks <= 100) {
+            grade = 'A';
+        } else if (averageMarks >= 70) {
+            grade = 'B';
+        } else if (averageMarks >= 50) {
+            grade = 'C';
+        } else if (averageMarks >= 40) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+
+        System.out.println();
+        System.out.println("Total marks of all subjects are: " + totalMarks);
+        System.out.printf("Average marks: %.2f%%\n", averageMarks);
+        System.out.println("Overall grade: " + grade);
+        System.out.println();
+
+        sc.close();
+    }
+}
